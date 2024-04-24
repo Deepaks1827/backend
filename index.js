80f2a9ff32d84c4bdfc2a5ef1350876ee034cc08
Deepaks1827 <deepak81unnao@gmail.com>
2024-04-24 07:34:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4500
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+res.send('Deepak Shukla')
+})
+app.get('/login',(req,res)=>{
+res.send('deepak ka login')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
